48fba6555333401deae1d6c18201998c4bd32cd9
Denis Korablev <dekor86@gmail.com>
2019-01-13 16:57:11 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Denis Korablev on 13/01/2019.
+//  Copyright © 2019 Denis Korablev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
